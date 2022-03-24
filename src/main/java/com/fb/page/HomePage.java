4386ef11a5931ffc947c5d03befdf31d95137575
akramwasimgithub <imwasim322@gmail.com>
2022-03-24 22:25:46 +0530

    Refactored

--- src/main/java/com/fb/page/HomePage.java
+++ src/main/java/com/fb/page/HomePage.java
@@ -7,13 +7,16 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BaseClass {
     @FindBy(xpath = "//span[contains(text(),'Raj Chavan')]")
-    WebElement userProfileLink;
+    WebElement userProfile;
 
     @FindBy(xpath = "//span[contains(text(),'Friends')]")
-    WebElement friendsLink;
+    WebElement findFriend;
 
     @FindBy(xpath = "//span[contains(text(),'Saved')]")
     WebElement savedLink;
+
+    @FindBy(xpath = "//span[contains(text(),'Groups')]")
+    WebElement groups;
 
     public HomePage() {
         PageFactory.initElements(driver, this);
@@ -23,10 +26,28 @@ public class HomePage extends BaseClass {
         return driver.getTitle();
     }
 
+    public String verifyHomePagePageTitle() {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return driver.getTitle();
+    }
+
     public ProfilePage clickOnProfile() {
-        userProfileLink.click();
+        userProfile.click();
         return new ProfilePage();
     }
 
+    public FindFriendsPage clickOnFriends() {
+        findFriend.click();
+        return new FindFriendsPage();
+    }
+
+    public GroupsPage clickOnGroups() {
+        groups.click();
+        return new GroupsPage();
+    }
 
 }
--- src/main/java/com/fb/page/ProfilePage.java
+++ src/main/java/com/fb/page/ProfilePage.java
@@ -7,5 +7,40 @@ import org.openqa.selenium.support.PageFactory;
 
 public class ProfilePage extends BaseClass {
 
+    @FindBy(xpath = "//span[contains(text(), 'About')]")
+    WebElement about;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/" +
+            "div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]" +
+            "/div[1]/a[3]/div[1]/span[1]")
+    WebElement friends;
+
+    @FindBy(xpath = "//span[contains(text(), 'Photos')]")
+    WebElement photos;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div" +
+            "[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]" +
+            "/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[5]/div[1]/span[1]")
+    WebElement videos;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]" +
+            "/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]" +
+            "/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[6]/div[1]/span[1]")
+    WebElement check;
+
+
+    public ProfilePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public String verifyProfilePageTitle() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return driver.getTitle();
+    }
+
 
 }
